b33c2905d3f020549bf94c1efea29482e7448d65
danknjoroge <dank8505@gmail.com>
2021-12-12 00:15:01 +0300

    added Readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Akan-Names-Generator
+## Akan-Names-Generator
+#### By Daniel Njoroge
+## Description
+This is web  page that generates Akan names.
+## Screenshot
+<img src="" alt="">
+<img src="" alt="">
+
+## Setup/Installation Requirements
+* Clone the repository to your computer from 
+     
+* Open the project using your preferred code editor, prefferably Visual Studio code.
+* Open the project using a web browser of your choice, preferably Google Chrome or if you have installed Liveserver in Visual Studio code, you can go live from there.
+## Live link
+     
+## Known Bugs
+There are no known bugs as per the time of publication. 
+## Technologies Used
+* HTML
+* CSS
+* Bootstrap
+* Javascript
+
+## Support and contact details
+For any queries or support you can reach me via the following email address:
+* dank8505@gmail.com
+### License
+This project is under [MIT](LICENSE).
--- script.js
+++ script.js
@@ -10,12 +10,17 @@ function validateForm() {
        alert( "Please provide your day of birth." );
        document.myForm.day.focus() ;
        return false;
-    }else if( document.myForm.month.value == "") {
-        alert( "Please, provide your month of birth." );
-       document.myForm.month.focus() ;
-       return false;
+    }else if( document.myForm.day.value>31 || document.myForm.day.value<=0){
+        alert( "Invalid date!! Date must be betweeen 1 and 31")
     }
-    else if( document.myForm.year.value == "") {
+    else if( document.myForm.month.value == "") {
+        alert( "Please provide your month of birth." );
+        document.myForm.month.focus() ;
+        return false;
+    }else if( document.myForm.month.value>12 || document.myForm.month.value<12) {
+        alert( "Invalid!! Month must be between 1 and 12")
+    }
+    else if(document.myForm.year.value == "") {
         alert( "Please provide your year of birth." );
         document.myForm.year.focus() ;
         return false;
